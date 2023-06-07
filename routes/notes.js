fc3cc0fac9d91f78be26d380b772edfd8c760eee
marvin blunck <marvinblunck@gmail.com>
2023-06-06 20:46:41 -0700

    server is running

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,5 @@
-const tips = require('express').Router();
-const uuid = require('uuid');
+const notes = require('express').Router();
+const uuid = require('../helpers/uuid');
 const { readFromFile,readAndAppend,writeToFile,} = require('../helpers/fsUtils');
 
 // GET Route for retrieving all the tips
@@ -7,22 +7,22 @@ notes.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// DELETE Route for a specific tip
-tips.delete('/:tip_id', (req, res) => {
-  const tipId = req.params.tip_id;
-  readFromFile('./db/tips.json')
-    .then((data) => JSON.parse(data))
-    .then((json) => {
-      // Make a new array of all tips except the one with the ID provided in the URL
-      const result = json.filter((tip) => tip.tip_id !== tipId);
+// // DELETE Route for a specific tip
+// tips.delete('/:tip_id', (req, res) => {
+//   const tipId = req.params.tip_id;
+//   readFromFile('./db/tips.json')
+//     .then((data) => JSON.parse(data))
+//     .then((json) => {
+//       // Make a new array of all tips except the one with the ID provided in the URL
+//       const result = json.filter((tip) => tip.tip_id !== tipId);
 
-      // Save that array to the filesystem
-      writeToFile('./db/tips.json', result);
+//       // Save that array to the filesystem
+//       writeToFile('./db/tips.json', result);
 
-      // Respond to the DELETE request
-      res.json(`Item ${tipId} has been deleted 🗑️`);
-    });
-});
+//       // Respond to the DELETE request
+//       res.json(`Item ${tipId} has been deleted 🗑️`);
+//     });
+// });
 
 // POST Route for a new UX/UI tip
 notes.post('/', (req, res) => {
